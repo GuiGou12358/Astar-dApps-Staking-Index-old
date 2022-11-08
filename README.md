d46067792c8568c22770eb16e97e22a931cdecba
= <=>
2022-11-08 16:24:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,156 @@
+# SubQuery - Index dApps and stakers for dApps Staking 
+
+This project index all data coming from dApps Staking
+
+## Preparation
+
+#### Environment
+
+- [Typescript](https://www.typescriptlang.org/) are required to compile project and define types.
+
+- Both SubQuery CLI and generated Project have dependencies and require [Node](https://nodejs.org/en/).
+
+#### Install
+
+Last, under the project directory, run following command to install all the dependency.
+
+```
+yarn install
+```
+
+## Configure your project
+
+You will be mainly working on the following files:
+
+- The Manifest in `project.yaml`
+- The GraphQL Schema in `schema.graphql`
+- The Mapping functions in `src/mappings/` directory
+
+For more information on how to write the SubQuery,
+check out our doc section on [Define the SubQuery](https://doc.subquery.network/define_a_subquery.html)
+
+#### Code generation
+
+In order to index your SubQuery project, it is mandatory to build your project first.
+Run this command under the project directory.
+
+```
+yarn codegen
+```
+
+## Build the project
+
+In order to deploy your SubQuery project to our hosted service, it is mandatory to pack your configuration before upload.
+Run pack command from root directory of your project will automatically generate a `your-project-name.tgz` file.
+
+```
+yarn build
+```
+
+## Indexing and Query
+
+#### Run required systems in docker
+
+Under the project directory run following command:
+
+```
+docker-compose pull && docker-compose up
+```
+
+#### Query the project
+
+Open your browser and head to `http://localhost:3000`.
+
+Finally, you should see a GraphQL playground is showing in the explorer and the schemas that ready to query.
+
+For the `subql-starter` project, you can try to query with the following code to get a taste of how it works.
+
+
+All stakers with their stake
+```graphql
+query{
+    accounts (
+        filter: { totalStake : {notEqualTo: "0"}}
+    ) {
+        totalCount
+        nodes{
+            id
+            totalStake
+        }
+    }
+}
+```
+
+Query given stakers
+```graphql
+query{
+    accounts (
+        filter:{
+            id:{
+                in: [
+                    "..."
+                ]
+            }
+        }
+    ){
+        nodes {
+            id
+            totalStake
+        }
+    }
+}
+```
+
+Some statistics
+```graphql
+query{
+    accounts (
+        filter: { totalStake : {notEqualTo: "0"}}
+    ) {
+        totalCount
+        aggregates {
+            sum {totalStake}
+            min {totalStake}
+            max {totalStake}
+            average {totalStake}
+        }
+    }
+}
+```
+
+All stakers with operations (stake/unstake/nominationTranfer)
+```graphql
+query{
+    accounts {
+        totalCount
+        nodes{
+            id
+            totalStake
+            stakes {
+                nodes {
+                    id
+                    amount
+                }
+            }
+            unstakes {
+                nodes {
+                    id
+                    amount
+                }
+            }
+            nominationTransferIn {
+                nodes {
+                    id
+                    amount
+                }
+            }
+            nominationTransferOut {
+                nodes {
+                    id
+                    amount
+                }
+            }
+        }
+    }
+}
+```
---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,186 @@
+import { SubstrateEvent } from "@subql/types";
+import { Balance } from "@polkadot/types/interfaces";
+import { AnyJson } from "@polkadot/types/types";
+import { DApp, Account, Stake, Reward } from "../types";
+
+function getDAppId(smartContract: AnyJson): string {
+	let evm = smartContract['evm'];
+  	if (evm) {
+    	return evm;
+  	}
+  	let wasm = smartContract['wasm'];
+  	if (wasm) {
+    	return wasm;
+  	}
+  	return null;
+}
+
+async function getDApp(dAppId: string): Promise<DApp> {
+   let dApp = await DApp.get(dAppId);
+    if (!dApp) {
+		dApp = new DApp(dAppId);
+		dApp.registered = false;
+    }
+    return dApp;
+}
+
+async function getAccount(accountId: string): Promise<Account> {
+    let account = await Account.get(accountId);
+    if (!account) {
+		account = new Account(accountId);
+    }
+  return account;
+}
+
+async function getStake(dAppId: string, accountId: string): Promise<Stake> {
+    let stakeId = `${dAppId}-${accountId}`;
+    let stake = await Stake.get(stakeId);
+    if (!stake) {
+		stake = new Stake(stakeId);
+        stake.dAppId = dAppId;
+        stake.accountId = accountId;
+		stake.totalStake = BigInt(0);
+    }
+  return stake;
+}
+
+async function getReward(dAppId: string, accountId: string): Promise<Reward> {
+    let rewardId = `${dAppId}-${accountId}`;
+    let reward = await Reward.get(rewardId);
+    if (!reward) {
+		reward = new Reward(rewardId);
+        reward.dAppId = dAppId;
+        reward.accountId = accountId;
+		reward.totalReward = BigInt(0);
+    }
+  return reward;
+}
+
+export async function registerContract(event: SubstrateEvent): Promise<void> {
+    const {
+        event: {
+            data: [account, smartContract],
+        },
+    } = event;
+
+    await logger.info("---------- DappsStaking - New Contract --------- ");
+
+    let dAppId = getDAppId(smartContract.toJSON());
+    let dApp = await getDApp(dAppId);
+	dApp.accountId = account.toString();
+	dApp.registered = true;
+	await dApp.save();
+}
+
+
+export async function unregisterContract(event: SubstrateEvent): Promise<void> {
+    const {
+        event: {
+            data: [account, smartContract],
+        },
+    } = event;
+
+    await logger.info("---------- DappsStaking - Contract Removed --------- ");
+
+    let dAppId = getDAppId(smartContract.toJSON());
+    let dApp = await getDApp(dAppId);
+	dApp.registered = false;
+	await dApp.save();
+}
+
+
+export async function bondAndStake(event: SubstrateEvent): Promise<void> {
+    const {
+        event: {
+            data: [account, smartContract, balanceOf],
+        },
+    } = event;
+
+    const amount = (balanceOf as Balance).toBigInt();
+
+    let dAppId = getDAppId(smartContract.toJSON());
+    let dApp = await getDApp(dAppId);
+	await dApp.save();
+
+    let accountId = account.toString();
+    let userAccount = await getAccount(accountId);
+	await userAccount.save();
+
+    let stake = await getStake(dAppId, accountId);
+	stake.totalStake += amount;
+	await stake.save();
+}
+
+
+export async function unbondAndUnstake(event: SubstrateEvent): Promise<void> {
+    const {
+        event: {
+            data: [account, smartContract, balanceOf],
+        },
+    } = event;
+
+    const amount = (balanceOf as Balance).toBigInt();
+
+    let dAppId = getDAppId(smartContract.toJSON());
+    let dApp = await getDApp(dAppId);
+	await dApp.save();
+
+    let accountId = account.toString();
+    let userAccount = await getAccount(accountId);
+	await userAccount.save();
+
+    let stake = await getStake(dAppId, accountId);
+	stake.totalStake -= amount;
+	await stake.save();
+}
+
+
+export async function nominationTransfer(event: SubstrateEvent): Promise<void> {
+    const {
+        event: {
+            data: [account, originSmartContract, balanceOf, targetSmartContract],
+        },
+    } = event;
+
+    const amount = (balanceOf as Balance).toBigInt();
+    let accountId = account.toString();
+
+    let originDAppId = getDAppId(originSmartContract.toJSON());
+    let originDApp = await getDApp(originDAppId);
+	await originDApp.save();
+
+    let originStake = await getStake(originDAppId, accountId);
+	originStake.totalStake -= amount;
+	await originStake.save();
+
+    let targetDAppId = getDAppId(targetSmartContract.toJSON());
+    let targetDApp = await getDApp(targetDAppId);
+	await targetDApp.save();
+
+    let targetStake = await getStake(targetDAppId, accountId);
+	targetStake.totalStake += amount;
+	await targetStake.save();
+
+}
+
+export async function reward(event: SubstrateEvent): Promise<void> {
+    const {
+        event: {
+            data: [account, smartContract, era, balanceOf],
+        },
+    } = event;
+
+    const amount = (balanceOf as Balance).toBigInt();
+
+    let dAppId = getDAppId(smartContract.toJSON());
+    let dApp = await getDApp(dAppId);
+	await dApp.save();
+
+    let accountId = account.toString();
+    let userAccount = await getAccount(accountId);
+	await userAccount.save();
+
+    let reward = await getReward(dAppId, accountId);
+	reward.totalReward += amount;
+	await reward.save();
+}
